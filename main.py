2664bb3d3a053ac88ba598cb1c5695054fc4954d
paul <pgtech246@gmail.com>
2024-03-12 14:33:57 -0400

    Send email with news titles, descriptions and urls

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import requests
+from send_email import send_email
+from datetime import date, timedelta
+
+current_date = date.today()
+ten_days_ago = current_date - timedelta(days=10)
 
 with open("api_key.txt", "r") as file:
     api_key = file.read()
 
-url = f"https://newsapi.org/v2/everything?q=AI&from=2024-02-11&sortBy=publishedAt&apiKey={api_key}"
+url = f"https://newsapi.org/v2/everything?q=AI&from={ten_days_ago}&sortBy=publishedAt&apiKey={api_key}&language=en"
 
 request = requests.get(url)
 content = request.json()
 
-for article in content["articles"]:
-    print("author: ", article["author"])
-    print("title: ", article["title"])
-    print("content: ", article["content"])
-    print("url: ", article["url"], "\n")
+body = ""
+for article in content['articles'][:11]:
+    body = body + str(article['title']) + '\n' + str(article['description']) + '\n' + str(article['url']) + 2*'\n'
+
+body = body.encode('utf-8')
+send_email(body)
